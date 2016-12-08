af33bc6412b9522b350f1a511b095d248b3c1387
harold101 <rossgreenhow@gmail.com>
2016-12-08 11:57:22 +0000

    this is work

--- tests/appSpec.js
+++ tests/appSpec.js
@@ -1,3 +1,24 @@
+
+describe("Learnjavacript", function (){
+  it ("can show question view" , function() {
+learnjavascript.showView("#quwstion-1");
+
+  }};
+
+
+}};
+
+
+
+
+
+
+
+
+
+
+
+
 /**
  * Created by davem on 27/11/2016.
  */
